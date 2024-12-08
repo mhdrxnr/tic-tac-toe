c28bf26c0e939747b93d8ae5f1851bc2f62d1f00
mohamed amamra <matrixrunner1@outlook.com>
2024-12-08 22:14:26 +0100

    button change

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -84,7 +84,7 @@ export default function Board() {
 
       <motion.button
       initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{duration: .3}}  whileHover={{scale: 1.25}}
-     onClick={handlePlayAgain} className="px-2 py-1 bg-gradient-to-r from-sky-600 to-purple-600  transition-transform duration-150 ease-in-out rounded-lg text-neutral-300 mt-4 font-bold">play Again</motion.button>
+     onClick={handlePlayAgain} className="px-2 py-1 bg-gradient-to-r from-sky-600 to-purple-600  rounded-lg text-neutral-300 mt-4 font-bold">play Again</motion.button>
       
     </div>
   )
